89e3bb3af6ad9c5d8095ab60fc5a2d460498eb36
ryansmith02 <ryaninbze12@gmail.com>
2025-02-13 13:15:35 +0400

    code for descriptive table and plot

--- Descriptive Tables.R
+++ Descriptive Tables.R
@@ -2,11 +2,13 @@ install.packages("haven")
 install.packages("dplyr")
 install.packages("labelled")
 install.packages("tableone")
+install.packages("ggplot2")
 
 library(haven)
 library(dplyr)
 library(labelled)
 library(tableone)
+library(ggplot2)
 
 endline <- read_dta("~/Downloads/endline_cleaned.dta")
 
@@ -26,15 +28,23 @@ endline <- endline %>%
   mutate(day2_missing = ifelse(is.na(day2), "Missing", "Not Missing"))
 
 endline <- endline %>%
-  mutate(across(c(region, wlthind, ethnic, currmarrstat, educ, religion, sex), as_factor))
+  mutate(across(c(district, wlthind, ethnicity, mstatus, educ, religion, sex, alive), as_factor))
 
-household_vars <- c("region", "wlthind")
-maternal_vars <- c("womanage", "ethnic", "currmarrstat", "educ", "religion")
-child_vars <- c("sex")
+household_vars <- c("district", "wlthind")
+maternal_vars <- c("womanage", "ethnicity", "mstatus", "mumedu", "religion")
+child_vars <- c("sex","alive")
 
-table <- CreateTableOne(vars = c(household_vars, maternal_vars, child_vars),
+table <- CreateTableOne(vars = c("district","wlthind","womanage","ethnicity",
+                                 "mstatus","mumedu","religion","sex","alive"),
                         strata = "day2_missing",  
                         data = endline, 
                         test = TRUE)  # Perform statistical tests
 
 print(table)
+
+endline %>%
+  filter(!is.na(district)) %>%
+ggplot(aes(x = district, fill = factor(day2_missing))) +
+  geom_bar(position = "dodge") +
+  labs(x = "District", y = "Number of Children", fill = "Day2 Missing") +
+  theme_minimal()
